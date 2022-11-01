35fe3bb277ac2c9f027c1a3d1d41433720453a50
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-11-01 13:42:41 +0530

    main

--- main.tf
+++ main.tf
@@ -1,55 +1,12 @@
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
-# Create AWS ec2 instance
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
+resource "aws_instance" "example" {
+  ami           = "ami-0e6329e222e662a52"
   instance_type = var.instance_type
-  security_groups= [var.security_group]
   tags= {
-    Name = var.tag_name
+    Name = "raham"
   }
 }
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
