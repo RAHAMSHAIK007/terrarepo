b317f9498de10578f52f35ea7f05296c2f39d282
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-11-01 13:33:13 +0530

    raham terra commitss

--- variable.tf
+++ variable.tf
@@ -3,6 +3,11 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "myJune2021Key"
+}
+
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
