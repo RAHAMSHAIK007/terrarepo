f950f95d11b665abd0df56f88f990b39f9d11840
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-11-01 13:23:42 +0530

    raham terra commits

--- variable.tf
+++ variable.tf
@@ -1,6 +1,25 @@
-variable "access_key" {
-  type = string
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
 }
-variable "secret_key" {
-  type = string
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
 }
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "RAHAM"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0e6329e222e662a52"
+}
+
